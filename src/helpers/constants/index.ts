341002944a86ec58b9c68724eef6cce66f718fbb
Emmanuel Ezekiel <emamex44@gmail.com>
2025-02-06 04:25:57 +0100

    first commit

--- src/helpers/constants/index.ts
+++ src/helpers/constants/index.ts
@@ -3,7 +3,7 @@ import { Filters } from "../types";
 export const DEFAULT_FILTERS: Filters = {
   query: "Senior Frontend Developers",
   country: "us",
-  date_posted: "all",
+  // date_posted: "all",
 };
 
 export const PAGE_SIZE = 10;
